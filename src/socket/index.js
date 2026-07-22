37bd0cf607c492909cacf30979228491a2a97b0e
oleg-insights <oleg.insights@mail.ru>
2026-07-22 11:50:06 +0300

    fix(socket): update port to match backend

--- src/socket/index.js
+++ src/socket/index.js
@@ -5,7 +5,7 @@ export const state = reactive({
     connected: false
 })
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000'
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3002'
 
 export const socket = io(URL)
 
